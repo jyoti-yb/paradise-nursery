9df4990c3034d336a516bd1737dc30ed6d85e7e9
Jyoti Yadav <balaboinjyotiyadav@gmail.com>
2025-03-31 20:29:26 +0530

    fixing bugs

--- src/index.js
+++ src/index.js
@@ -1,12 +1,11 @@
+import { BrowserRouter } from "react-router-dom";
 import React from "react";
 import ReactDOM from "react-dom";
-import { Provider } from "react-redux";
-import store from "./redux/store";
 import App from "./App";
 
 ReactDOM.render(
-  <Provider store={store}>
+  <BrowserRouter basename="/paradise-nursery">
     <App />
-  </Provider>,
+  </BrowserRouter>,
   document.getElementById("root")
 );
